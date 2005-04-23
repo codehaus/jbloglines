316a5a298648ed7c31155d76f6bb98fcfe086eac
joneswm <joneswm@1b51b317-b372-0410-ad90-c813ece56701>
2005-04-23 08:07:00 +0000

    Rename classes: *TestCase to *Test

---
+++ jbloglines/test/src/org/codehaus/bloglines/BloglinesTest.java
@@ -0,0 +1,85 @@
+package org.codehaus.bloglines;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import org.codehaus.bloglines.exceptions.BloglinesException;
+import org.codehaus.bloglines.http.BloglinesRestCaller;
+import org.codehaus.bloglines.unmarshall.ItemsUnmarshall;
+import org.codehaus.bloglines.unmarshall.OutlineUnmarshal;
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+
+import java.util.Date;
+
+public class BloglinesTest extends MockObjectTestCase {
+    private Mock restCallerMock;
+    private Mock outlineMarshallMock;
+    private Mock itemUnmarshallMock;
+    private Bloglines bloglines;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        restCallerMock = mock(BloglinesRestCaller.class);
+        outlineMarshallMock = mock(OutlineUnmarshal.class);
+        itemUnmarshallMock = mock(ItemsUnmarshall.class);
+
+        bloglines = new Bloglines((OutlineUnmarshal) outlineMarshallMock.proxy(),
+                                  (ItemsUnmarshall) itemUnmarshallMock.proxy(),
+                                  (BloglinesRestCaller) restCallerMock.proxy());
+    }
+
+    public void testSettingCredentialsCallsThroughToTheRestApi() {
+        String username = "sakfhaksf";
+        String password = "dsakufhasfh";
+
+        restCallerMock.expects(once()).method("setCredentials").with(same(username), same(password));
+
+        bloglines.setCredentials(username, password);
+
+    }
+
+    public void testListSubscriptionsCallsTheRestApiAndUnmarshallsTheResults() throws BloglinesException {
+        Outline expectedSubscriptionOutline = new Outline();
+        String bloglinesRestResponse = "This is the response from Bloglines";
+        restCallerMock.expects(once()).method("call").with(eq("listsubs"), NULL).will(returnValue(bloglinesRestResponse));
+        outlineMarshallMock.expects(once()).method("unmarshal").with(same(bloglinesRestResponse)).will(returnValue(expectedSubscriptionOutline));
+
+        Outline subscriptionOutline = bloglines.listSubscriptions();
+
+        assertSame(expectedSubscriptionOutline, subscriptionOutline);
+    }
+
+    public void testGetItemsCallsTheRestApiAndUnmarshallsTheResults() throws BloglinesException {
+        SyndFeed expectedSyndFeed = (SyndFeed) mock(SyndFeed.class).proxy();
+        String bloglinesRestResponse = "This is the response from Bloglines";
+        String subscriptionId = "343485";
+        String[] parameters = new String[]{"s", subscriptionId, "n", "0"};
+        restCallerMock.expects(once()).method("call").with(eq("getitems"), eq(parameters)).will(returnValue(bloglinesRestResponse));
+        itemUnmarshallMock.expects(once()).method("unmarshal").with(same(bloglinesRestResponse)).will(returnValue(expectedSyndFeed));
+
+        Date startDate = new Date();
+        Outline outline = new Outline();
+        outline.setSubscriptionId(subscriptionId);
+
+        SyndFeed syndFeed = bloglines.getItems(outline, false, startDate);
+
+        assertSame(expectedSyndFeed, syndFeed);
+    }
+
+    public void testGetItemsCallsTheRestApiPassingThroughTheMarkAsReadFlag() throws BloglinesException {
+        SyndFeed expectedSyndFeed = (SyndFeed) mock(SyndFeed.class).proxy();
+        String bloglinesRestResponse = "This is the response from Bloglines";
+        String subscriptionId = "343485";
+        String[] parameters = new String[]{"s", subscriptionId, "n", "1"};
+        restCallerMock.expects(once()).method("call").with(eq("getitems"), eq(parameters)).will(returnValue(bloglinesRestResponse));
+        itemUnmarshallMock.expects(once()).method("unmarshal").with(same(bloglinesRestResponse)).will(returnValue(expectedSyndFeed));
+
+        Date startDate = new Date();
+        Outline outline = new Outline();
+        outline.setSubscriptionId(subscriptionId);
+
+        SyndFeed syndFeed = bloglines.getItems(outline, true, startDate);
+
+        assertSame(expectedSyndFeed, syndFeed);
+    }
+
+}
---
+++ jbloglines/test/src/org/codehaus/bloglines/ItemsUnmarshallTest.java
@@ -0,0 +1,111 @@
+/*
+ * Created on Dec 9, 2004
+ *
+ * 
+ * Copyright (c) Zohar Melamed All rights reserved.
+ * 
+ * 
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. Due credit should be given to The Codehaus and Contributors
+ * http://timtam.codehaus.org/
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
+ *  
+ */
+package org.codehaus.bloglines;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import org.codehaus.bloglines.exceptions.BloglinesException;
+import org.codehaus.bloglines.unmarshall.ItemsUnmarshall;
+import org.codehaus.bloglines.unmarshall.ItemsUnmarshallImpl;
+import org.jmock.Mock;
+import org.jmock.cglib.MockObjectTestCase;
+import org.jmock.core.Constraint;
+
+import java.io.StringReader;
+
+public class ItemsUnmarshallTest extends MockObjectTestCase {
+    public void testUnmarshallUsesSyndInputFeedToBuildSyndFeed() throws Exception {
+        String items = "Items to unmarshall";
+        Mock syndFeedInputMock = mock(SyndFeedInput.class);
+        SyndFeed expectedSyndFeed = (SyndFeed) mock(SyndFeed.class).proxy();
+        syndFeedInputMock.expects(once()).method("build").with(eqStringReader(items)).will(returnValue(expectedSyndFeed));
+
+        ItemsUnmarshall itemUnmarshall = new ItemsUnmarshallImpl((SyndFeedInput) syndFeedInputMock.proxy());
+
+        SyndFeed syndFeed = itemUnmarshall.unmarshal(items);
+
+        assertSame(expectedSyndFeed, syndFeed);
+    }
+
+    public void testUnmarshallWrapsExceptionAsBloglinesException() throws Exception {
+        String items = "Items to unmarshall";
+        Mock syndFeedInputMock = mock(SyndFeedInput.class);
+        Throwable thrownException = new IllegalArgumentException();
+        syndFeedInputMock.expects(once()).method("build").with(eqStringReader(items)).will(throwException(thrownException));
+
+        ItemsUnmarshall itemUnmarshall = new ItemsUnmarshallImpl((SyndFeedInput) syndFeedInputMock.proxy());
+
+        try {
+            itemUnmarshall.unmarshal(items);
+            fail("Sould have thrown exception");
+        } catch (BloglinesException e) {
+            assertEquals("umarshalling items failed", e.getMessage());
+            assertSame(thrownException, e.getCause());
+        }
+
+    }
+
+    private class StringReaderContentsConstraint implements Constraint {
+        private String expectedStringContents;
+
+        public StringReaderContentsConstraint(String expectedStringContents) {
+            this.expectedStringContents = expectedStringContents;
+        }
+
+        public boolean eval(Object o) {
+            try {
+                StringReader stringReader = (StringReader) o;
+                char[] chars = new char[expectedStringContents.length() + 5];
+                int length = stringReader.read(chars);
+                String content = new String(chars, 0, length);
+                return content.equals(expectedStringContents);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        public StringBuffer describeTo(StringBuffer stringBuffer) {
+            return stringBuffer.append("<StringReader:{" + expectedStringContents + "}>");
+        }
+    }
+
+    private Constraint eqStringReader(String items) {
+        Constraint stringReaderContents = new StringReaderContentsConstraint(items);
+        return stringReaderContents;
+    }
+
+}
