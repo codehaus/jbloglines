662768a3cefe1b8fbd4091a4e6adc9f66ccdf0fa
joneswm <joneswm@1b51b317-b372-0410-ad90-c813ece56701>
2005-04-25 21:36:32 +0000

    Added versioning and testing into build.xml

--- trunk/jbloglines/test/src/org/codehaus/bloglines/BloglinesAcceptanceTests.java
+++ trunk/jbloglines/test/src/org/codehaus/bloglines/BloglinesAcceptanceTests.java
@@ -1,3 +1,38 @@
+/*
+ * Created on Nov 24, 2004
+ *
+ *
+ * Copyright (c) Zohar Melamed All rights reserved.
+ *
+ *
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright statements and
+ * notices. Redistributions must also contain a copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Due credit should be given to The Codehaus and Contributors
+ * http://timtam.codehaus.org/
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ *
+ */
 package org.codehaus.bloglines;
 
 import com.sun.syndication.feed.synd.SyndFeed;
@@ -11,51 +46,12 @@ import org.codehaus.bloglines.http.HttpMethodFactoryImpl;
 import org.codehaus.bloglines.unmarshal.ItemsUnmarshallerImpl;
 import org.codehaus.bloglines.unmarshal.OutlineUnmarshallerImpl;
 
-/*
- * Created on Nov 24, 2004
- *
- * 
- * Copyright (c) Zohar Melamed All rights reserved.
- * 
- * 
- * Redistribution and use of this software and associated documentation
- * ("Software"), with or without modification, are permitted provided that the
- * following conditions are met:
- * 
- * 1. Redistributions of source code must retain copyright statements and
- * notices. Redistributions must also contain a copy of this document.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * 
- * 3. Due credit should be given to The Codehaus and Contributors
- * http://timtam.codehaus.org/
- * 
- * THIS SOFTWARE IS PROVIDED BY THE CODEHAUS AND CONTRIBUTORS ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE CODEHAUS OR ITS CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * 
- *  
- */
-
-/**
- * @author Zohar
- */
 // we expect :
 //<outline title="Subscriptions">
-//<outline title="Bloglines | News" htmlUrl="http://www.bloglines.com" type="rss" xmlUrl="http://www.bloglines.com/rss/about/news"  BloglinesSubId="5259174"  BloglinesUnread="8" BloglinesIgnore="0" />
-//<outline title="Stuff" BloglinesSubId="5259181" BloglinesIgnore="0">
-//    <outline title="Boing Boing" htmlUrl="http://www.boingboing.net/" type="rss" xmlUrl="http://boingboing.net/rss.xml"  BloglinesSubId="5259182"  BloglinesUnread="26" BloglinesIgnore="0" />
-//</outline>
+//    <outline title="Bloglines | News" htmlUrl="http://www.bloglines.com" type="rss" xmlUrl="http://www.bloglines.com/rss/about/news"  BloglinesSubId="5259174"  BloglinesUnread="8" BloglinesIgnore="0" />
+//    <outline title="Stuff" BloglinesSubId="5259181" BloglinesIgnore="0">
+//        <outline title="Boing Boing" htmlUrl="http://www.boingboing.net/" type="rss" xmlUrl="http://boingboing.net/rss.xml"  BloglinesSubId="5259182"  BloglinesUnread="26" BloglinesIgnore="0" />
+//    </outline>
 //</outline>
 
 public class BloglinesAcceptanceTests extends TestCase {
@@ -65,7 +61,9 @@ public class BloglinesAcceptanceTests extends TestCase {
 
     protected void setUp() throws Exception {
         super.setUp();
-        bloglines = new Bloglines(new OutlineUnmarshallerImpl(), new ItemsUnmarshallerImpl(new SyndFeedInput()), new BloglinesRestCallerImpl(new HttpCallerImpl(new HttpClient(), new HttpMethodFactoryImpl()), "UTF-8"));
+        bloglines = new Bloglines(new OutlineUnmarshallerImpl(),
+                                  new ItemsUnmarshallerImpl(new SyndFeedInput()),
+                                  new BloglinesRestCallerImpl(new HttpCallerImpl(new HttpClient(), new HttpMethodFactoryImpl()), "UTF-8"));
     }
 
     public void testListSubs() throws BloglinesException {
@@ -77,9 +75,12 @@ public class BloglinesAcceptanceTests extends TestCase {
 
         Outline[] outlines = topLevel.getChildren();
         assertEquals(2, outlines.length);
+
         Outline stuff = outlines[1];
         assertEquals("Stuff", stuff.getTitle());
         assertEquals(1, stuff.getChildren().length);
+        assertEquals(false, stuff.isFeed());
+
         Outline boingboing = stuff.getChildren()[0];
         assertEquals("Boing Boing", boingboing.getTitle());
         assertTrue(boingboing.isFeed());
